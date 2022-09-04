9c029faf5742c84e09a0093345d956a4cc86a69f
Om Patil <kushpo.030507@gmail.com>
2022-09-04 23:48:12 +0530

    day 6

---
+++ day6.cpp
@@ -0,0 +1,31 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int T;
+    cin >> T;
+
+    for (int i = 0; i < T; i++) {
+        string str;
+        cin >> str;
+
+        for (int j = 0; j < str.length(); j++) {
+            if (j % 2 == 0) cout << str[j];
+        }
+
+        cout << " ";
+
+        for (int j = 0; j < str.length(); j++) {
+            if (j % 2 != 0) cout << str[j];
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
