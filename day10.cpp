5a37c672d45edb7a690f679a6dc727827ef7fbc2
Om Patil <kushpo.030507@gmail.com>
2022-09-08 15:37:14 +0530

    day 10

---
+++ day10.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+
+
+int main()
+{
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+    int m=0,i=0;
+    while (n!=0) {
+        if (n%2)
+            i++;
+        else
+            i = 0;
+        if (i>m)
+            m=i;
+        n=n/2;
+    }
+    cout<<m<<endl;
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
