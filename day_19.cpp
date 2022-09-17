7dcdedb9dd058b6d061560dec021fdaeb30385e2
Om Patil <kushpo.030507@gmail.com>
2022-09-17 22:20:55 +0530

    day 19

---
+++ day_19.cpp
@@ -0,0 +1,32 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+class AdvancedArithmetic{
+    public:
+        virtual int divisorSum(int n)=0;
+};
+class Calculator : public AdvancedArithmetic {
+public:
+    int divisorSum(int n) {
+        int i = 1, j = 0;
+        while (i <= n / 2) {
+            if (!(n % i))
+                j = j + i;
+            i++;
+        }
+        return j + n;
+    }
+};
+
+int main(){
+    int n;
+    cin >> n;
+    AdvancedArithmetic *myCalculator = new Calculator(); 
+    int sum = myCalculator->divisorSum(n);
+    cout << "I implemented: AdvancedArithmetic\n" << sum;
+    return 0;
+}
