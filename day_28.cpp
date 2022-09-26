1efd3b52968436ca4862d2576ebe953a06e2204c
Om Patil <kushpo.030507@gmail.com>
2022-09-26 20:34:14 +0530

    day 28

---
+++ day_28.cpp
@@ -0,0 +1,91 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+
+
+int main()
+{
+    string N_temp, last;
+    vector<string> order;
+    getline(cin, N_temp);
+
+    int N = stoi(ltrim(rtrim(N_temp)));
+
+    for (int N_itr = 0; N_itr < N; N_itr++) {
+        string first_multiple_input_temp;
+        getline(cin, first_multiple_input_temp);
+
+        vector<string> first_multiple_input = split(rtrim(first_multiple_input_temp));
+
+        string firstName = first_multiple_input[0];
+
+        string emailID = first_multiple_input[1];
+        
+        last = "@gmail.com";
+        
+        int i = 0, size;
+        size = emailID.size() - 10;
+        bool check = true;
+        
+        while ((emailID[size + i] != 0) || last[i] != 0 ) {
+            if (emailID[size + i] != last[i]){
+                check = false;
+                break;
+            }
+            i ++;
+        }
+        if (check) {
+            order.push_back(firstName);
+        }
+    }
+    sort(order.begin(), order.end());
+    
+    for (int j = 0; j < order.size(); j++)
+        cout<<order[j]<<endl;
+    
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
